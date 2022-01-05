eadf2137e2871050a53f866d6df1458540fbc989
cfgexe <65036495+cfgexe@users.noreply.github.com>
2022-01-05 14:57:33 +1100

    Update Updater.py

--- tools/Updater.py
+++ tools/Updater.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 # remember to change this and the one in version.txt everytime you update
 version = "v6.1"
 def checkForUpdates():
-    r = urlopen("https://raw.githubusercontent.com/MaleVTuber/CircleRGB/master/version.txt")
+    r = urlopen("https://raw.githubusercontent.com/cfgexe/CircleRGB/master/version.txt")
                                      
     for line in r:
         if line != version:
